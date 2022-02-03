469834bf333990398aafbde4433f6dc9ffff9f20
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-02-03 22:07:04 +0530

    Add files via upload

---
+++ Function.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void function();
+void secondFunction();
+
+int main() {
+
+    function();
+
+    secondFunction();
+
+    cout << "Hello from main()\n";
+
+    function();
+
+    return 0;
+}
+
+void function() {
+    cout << "Hello from function()" << endl;
+}
+
+void secondFunction() {
+    cout << "Hello, This is from secondFunction()";
+}
