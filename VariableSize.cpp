d789da810f444f97db93d6d4b86680dce9adb1e5
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-02-10 22:18:51 +0530

    Add files via upload

---
+++ VariableSize.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    cout << "Size of integer: " << sizeof(int) << " Bytes" << endl;
+    cout << "Size of charater: " << sizeof(char) << " Byte" << endl;
+    cout << "Size of floating: " << sizeof(float) << " Bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " Bytes" << endl;
+    return 0;
+}
