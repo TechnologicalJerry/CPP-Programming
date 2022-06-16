2a5c0c6521db4884ea92d2f30d02aa2f8de05d70
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-06-16 23:08:01 +0530

    update

---
+++ Swap.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int ss = 9;
+  int sg = 14;
+  cout << "Value of ss before swapping: " << ss << endl;
+  cout << "Value of sg before swapping: " << sg << endl;
+  swap(ss, sg);
+  cout << "Value of ss after swapping: " << ss << endl;
+  cout << "Value of sg after swapping: " << sg << endl;
+  return 0;
+}
